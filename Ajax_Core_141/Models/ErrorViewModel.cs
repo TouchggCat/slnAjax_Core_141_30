3cfd48bc6a33e6e22359c8c7f8451b670a3271ec
TouchggCat <giraffegod@gmail.com>
2022-07-02 16:47:06 +0800

    加入專案檔案。

---
+++ Ajax_Core_141/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ajax_Core_141.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
